e3c69855ca3abdf8f008883cdfc1736fe532f6a5
AlexOrellana <alexore03@unitec.edu>
2016-02-12 14:37:26 -0600

    metodo asignar naves agregado

--- Laboratorio 4.cpp
+++ Laboratorio 4.cpp
@@ -6,6 +6,7 @@ using std::endl;
 
 void borrar(int*** arreglo[], int size);
 int*** crear();
+void AsignarNaves(int*** x);
 
 int main(int argc, char*argv[]){
 	int*** arreglo1 = crear();
@@ -31,4 +32,31 @@ int*** crear(){
 		for (int j = 0; j < size; j++)
 			arreglo[i][j] = new int[size];
 	return arreglo;
+}
+void AsignarNaves(int*** x){
+
+	int naves=15,ran1,ran2,ran3;
+
+	for(int i=0;i<12;i++){
+		for(int j=0;j<12;j++){
+			for(int w=0;w<12;w++){
+				x[i][j][w]=1;
+			}	
+		}
+	}
+
+	while(naves!=0){
+
+		srand(time(NULL));
+		ran1 = rand()%12+0;
+		ran2 = rand()%12+0;
+		ran3 = rand()%12+0;		
+
+		if(x[ran1][ran2][ran3]==1){
+			x[ran1][ran2][ran3]==2;
+			naves--;
+		}else{
+
+		}
+	}	
 }
